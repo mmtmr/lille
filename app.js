4750b26641696e8dcca884e9851b10dbf6a9415a
Maxine <ms.l@hotmail.com>
2021-10-21 19:56:09 +0800

    fix time log created time missing issue

--- app.js
+++ app.js
@@ -322,9 +322,9 @@ app.delete('/api/task/:tsk_id/:st_id', async (req, res) => {
 //Create a timelog
 app.post('/api/timelog', async (req, res) => {
     try {
-        const { tl_date, tl_standby_min, tl_real_min, tl_created, tsk_id, st_ids } = req.body;
+        const { tl_date, tl_standby_min, tl_real_min, tsk_id, st_ids } = req.body;
         const newTimeLog = await pool.query(
-            "INSERT INTO time_log_t (tl_date,tl_standby_min,tl_real_min,tl_created,tsk_id) VALUES ($1,$2,$3,$4,$5) RETURNING tl_id", [tl_date, tl_standby_min, tl_real_min, tl_created, tsk_id]
+            "INSERT INTO time_log_t (tl_date,tl_standby_min,tl_real_min,tsk_id) VALUES ($1,$2,$3,$4,$5) RETURNING tl_id", [tl_date, tl_standby_min, tl_real_min, tsk_id]
         );
         const { tl_id } = newTimeLog.rows[0];
 
