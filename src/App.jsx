fe2aee3607a8b256dd9dd841f3226d164f2b2bb3
Maxine <ms.l@hotmail.com>
2021-11-24 09:58:26 +0800

    fix toast notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { AppRouter } from './routers/AppRouter';
-
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 export const App=()=>(
+        <>
         <AppRouter />
+        <ToastContainer autoClose={5000}/>
+        </>
 )
