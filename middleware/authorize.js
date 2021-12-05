203ab5d51abacc75e9792b6513b491d405adf99b
Maxine <ms.l@hotmail.com>
2021-12-05 22:22:59 +0800

    fix refreshToken not giving token issue

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 //this middleware will on continue on if the token is inside the local storage
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   // Get token from header
   var token = req.header("jwt_token");
   const refreshToken = req.header("rt_token");
@@ -12,31 +12,29 @@ module.exports = function(req, res, next) {
   // Check if not token
   if (!token && !refreshToken) {
     return res.status(403).json({ msg: "authorization denied" });
-  } else if (!token && refreshToken){
-
-    //Verify refreshToken
+  } else {
+    // Verify token
     try {
       //it is going to give user the user id (user:{id: user.id})
-      const verifyRefresh = jwt.verify(refreshToken, process.env.rtSecret);
-      req.user = verifyRefresh.user;
-
-      token = jwtGenerator(req.user.user_id);
-      req.jwtToken=token;
+      
+      const verify = jwt.verify(token, process.env.jwtSecret);
+      req.user = verify.user;
+      req.jwtToken = token;
       next();
     } catch (err) {
-      res.status(401).json({ msg: "Refresh Token is not valid" });
+      //Verify refreshToken
+      try {
+        //it is going to give user the user id (user:{id: user.id})
+        const verifyRefresh = jwt.verify(refreshToken, process.env.rtSecret);
+        req.user = verifyRefresh.user;
+
+        token = jwtGenerator(req.user.user_id);
+        req.jwtToken = token;
+        next();
+      } catch (error) {
+        res.status(401).json({ msg: "Refresh Token is not valid" });
+      }
     }
-  }else if(token){
-     // Verify token
-  try {
-    //it is going to give user the user id (user:{id: user.id})
-    const verify = jwt.verify(token, process.env.jwtSecret);
-    req.user = verify.user;
-    req.jwtToken=token;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
-  }
   }
 
 };
